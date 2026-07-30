4d1b0359d72715878b1d27da3ebca0883e2f08c4
Jihyeon Kim <simnalamburt@gmail.com>
2026-07-30 17:45:46 +0900

    Ditch custom sitemap

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,8 +1,10 @@
+import sitemap from '@astrojs/sitemap'
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
   site: 'https://blog.langdev.org',
   trailingSlash: 'never',
+  integrations: [sitemap()],
   markdown: {
     syntaxHighlight: false,
   },
